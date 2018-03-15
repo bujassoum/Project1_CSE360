3c92d24056e6220dfc29e955f3bde79604212c1e
Mohamed Bu Jassoum <35940911+bujassoum@users.noreply.github.com>
2018-03-15 04:35:18 -0700

    Update StringListener.java

--- StringListener.java
+++ StringListener.java
@@ -2,7 +2,7 @@ package project1;
 
 public interface StringListener {
 	
-	public void textEmitted(String text);
-	public void textEmitted1(String text); 
+	public void textEmitted(String text); // to display into input textArea
+	public void textEmitted1(String text); // to dispaly into output textArea
 
 }
