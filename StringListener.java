4c5fbd675b94992b4cb9e4e30ac6f29f42d4e10d
Ishkablob <braydonshadow@gmail.com>
2018-03-16 12:45:13 -0700

    VChange some

--- StringListener.java
+++ StringListener.java
@@ -4,5 +4,6 @@ public interface StringListener {
 	
 	public void textEmitted(String text); // to display into input textArea
 	public void textEmitted1(String text); // to dispaly into output textArea
+	public void clearText();
 
 }
